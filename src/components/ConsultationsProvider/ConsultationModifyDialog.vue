4dd11e9237f76d4c0f292412c4a32662181dfb59
Timon Dubreuil <timondubreuil@outlook.com>
2024-08-10 17:53:35 +0200

    ajout localisation fr

--- src/components/ConsultationsProvider/ConsultationModifyDialog.vue
+++ src/components/ConsultationsProvider/ConsultationModifyDialog.vue
@@ -2,8 +2,21 @@
     <v-dialog v-model="dialogVisible" persistent>
         <v-card>
             <v-card-title>{{ consultation.nom }}</v-card-title>
+            <v-card-text>
+                <v-row>
+                    <v-col cols="12">
+                            <ConsultationsTypeSelect v-model="typeConsultation"></ConsultationsTypeSelect>
+                    </v-col>
 
-            <v-card-text></v-card-text>
+                    <v-col cols="6">
+                        <v-date-input first-day-of-week="1" label="Date de la consultation" placeholder="jj/mm/aaaa"></v-date-input>
+                    </v-col>
+
+                    <v-col cols="6">
+                        <v-time-picker ></v-time-picker>
+                    </v-col>
+                </v-row>
+            </v-card-text>
 
             <v-card-actions>
                 <v-row>
@@ -19,8 +32,16 @@
 </template>
 
 <script>
+    import ConsultationsTypeSelect from '../Consultations/ConsultationsTypeSelect.vue';
+    import { VTimePicker } from 'vuetify/labs/VTimePicker'
+    import { VDateInput } from 'vuetify/labs/VDateInput'
     export default{
         name:"ConsultationModifyDialog",
+        components:{
+            ConsultationsTypeSelect,
+            VTimePicker,
+            VDateInput
+        },
         props:{
             consultation:{
                 type:Object,
@@ -29,6 +50,15 @@
             modelValue:{
                 type:Boolean,
                 required:true
+            }
+        },
+        watch:{
+            consultation(newValue)
+            {
+                // console.log("nv : "+JSON.stringify(newValue));
+                const typeId = newValue.typeid
+                // console.log("ti : "+typeId);
+                this.typeConsultation = typeId
             }
         },
         computed:{
@@ -52,7 +82,7 @@
         },
         data(){
             return{
-
+                typeConsultation:null
             }
         }
     }
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,7 +4,14 @@ import 'vuetify/styles'
 
 // Vuetify
 import { createVuetify } from 'vuetify'
+import {fr} from 'vuetify/locale'
 
-export default createVuetify(
+export default createVuetify({
+    locale: {
+      locale:'fr',
+      fallback:'en',
+      messages:{fr}
+    }
+  }
   // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 )
