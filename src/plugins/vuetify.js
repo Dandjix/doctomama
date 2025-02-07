2912826684e5476e9ec48599f8ca7513be909fa8
Timon Dubreuil <timondubreuil@outlook.com>
2025-02-07 19:30:58 +0100

    créé theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,11 +6,57 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import {fr} from 'vuetify/locale'
 
+const doctomamaLight = {
+  dark: false,
+  colors: {
+    background: '#FFFFFF',
+    surface: '#FFFFFF',
+    'surface-bright': '#FFFFFF',
+    'surface-light': '#EEEEEE',
+    'surface-variant': '#424242',
+    'on-surface-variant': '#EEEEEE',
+    primary: '#E56399',
+    'primary-darken-1': '#1F5592',
+    secondary: '#4C1A57',
+    'secondary-darken-1': '#018786',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  },
+  variables: {
+    'border-color': '#000000',
+    'border-opacity': 0.12,
+    'high-emphasis-opacity': 0.87,
+    'medium-emphasis-opacity': 0.60,
+    'disabled-opacity': 0.38,
+    'idle-opacity': 0.04,
+    'hover-opacity': 0.04,
+    'focus-opacity': 0.12,
+    'selected-opacity': 0.08,
+    'activated-opacity': 0.12,
+    'pressed-opacity': 0.12,
+    'dragged-opacity': 0.08,
+    'theme-kbd': '#212529',
+    'theme-on-kbd': '#FFFFFF',
+    'theme-code': '#F5F5F5',
+    'theme-on-code': '#000000',
+  }
+}
+
+
+
 export default createVuetify({
     locale: {
       locale:'fr',
       fallback:'en',
       messages:{fr}
+    },
+    theme:{
+      defaultTheme:'doctomamaLight',
+      themes:{
+        doctomamaLight
+      }
     }
   }
   // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
